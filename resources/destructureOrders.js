6e3f14a8ec9d818d846871ea3bb07210865bbb8a
Tim <Tyangpersonal1@gmail.com>
2026-06-16 15:53:25 +1200

    feat: further reduced file size inside the s3 bucket
    TODO:
    can now begin the order analysis portion

--- resources/destructureOrders.js
+++ resources/destructureOrders.js
@@ -1,10 +1,41 @@
 export const destructureOrders = async (ordersJson) => {
-    const result = responseJson.data.filter(item => item.user.status == 'ingame' || item.user.status == 'online').map(({ platinum, quantity, type, user }) => ({
+    const result = ordersJson.data.filter(item => item.user.status == 'ingame' || item.user.status == 'online').map(({ platinum, quantity, type, user }) => ({
       platinum: platinum,
       quantity: quantity,
       type : type,
       lastSeen : user.lastSeen,
       status : user.status
     }));
-    return result;
+    const twoDaysAgo = new Date();
+    twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
+
+    const filteredResults = result.filter(item => {
+      const lastSeenDate = new Date(item.lastSeen);
+      
+      return (lastSeenDate > twoDaysAgo);
+    });
+
+
+    const counts = filteredResults.reduce((acc, curr) => {
+      const key = curr.platinum; 
+      if (!acc[key]) {
+        acc[key] = 0
+      }
+
+      if (curr.type == 'buy'){
+        acc[key] -= 1;
+      } else {
+        acc[key] += 1;
+      }
+      return acc;
+    }, {});
+
+  const reportJson = Object.entries(counts)
+    .sort((a, b) => a - b)
+    .map(([price, count]) => ({
+      price: Number(price),
+      orderCount: count
+    }));
+
+  return reportJson;
 }
