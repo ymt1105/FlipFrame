3af2836fedc3fca4e2fe5ea9b603e44f71ec3542
Tim <Tyangpersonal1@gmail.com>
2026-08-05 16:10:06 +1200

    feat: redundant code deleted, reorganised folder structure and small ux improvements

--- frontend/flipframe/src/features/rivens/RivenCard.jsx
+++ frontend/flipframe/src/features/rivens/RivenCard.jsx
@@ -14,7 +14,6 @@ export const RivenCard = ({rivenData}) => {
     }
     rivenDetails.weapon_name = String(rivenDetails.weapon_name).charAt(0).toUpperCase() + String(rivenDetails.weapon_name).slice(1);
     let counter = 0
-    console.log(rivenData);
     return(
         <div className="bg-white p-4 border rounded shadow-md flex flex-col h-[300px] w-[550px] shrink-0 overflow-hidden">        
             <h2 className="center text-lg font-bold mb-2">{rivenDetails.weapon_name} {rivenDetails.prefix}</h2>
